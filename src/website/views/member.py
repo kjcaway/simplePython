cfc9ba56a64e184b5ae8d70233510d9fd0abcb86
kjcaway <whdckstngh@naver.com>
2018-10-21 01:02:53 +0900

    add delete member

--- src/website/views/member.py
+++ src/website/views/member.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, g
 from .. import db
+from .. import mylogger
+import json
 
 blueprint = Blueprint('member', __name__)
 
@@ -7,8 +9,24 @@ blueprint = Blueprint('member', __name__)
 def index():
   # To do
   members = db.members.find()
-
-
+  
   return render_template('member/list.html', members = members)
+
+@blueprint.route('/<userid>', methods = ['DELETE'])
+def remove(userid):
+  # To do
+  mylogger.info(userid + ' is deleted')
+
+  result = {
+    "status" : "success"
+  }
+
+  try:
+    db.members.delete_one({"userid":userid})
+  except Exception as e:
+    mylogger.error(e)
+    result['status'] = 'fail'
+
+  return json.dumps(result)
 
 
