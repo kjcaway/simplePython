aba0ddcd5c857173af5b672243adba3e838219f8
kjcaway <whdckstngh@naver.com>
2018-10-22 23:15:13 +0900

    add modal, add modify member

--- src/website/views/member.py
+++ src/website/views/member.py
@@ -2,19 +2,55 @@ from flask import Blueprint, render_template, request, redirect, g
 from .. import db
 from .. import mylogger
 import json
+from pymongo import ReturnDocument
 
 blueprint = Blueprint('member', __name__)
 
 @blueprint.route('/')
 def index():
-  # To do
   members = db.members.find()
   
   return render_template('member/list.html', members = members)
 
+@blueprint.route('/<userid>', methods = ['GET'])
+def getMember(userid):
+  member = {
+    'userid': '',
+    'username' : '',
+  }
+
+  try:
+    ret = db.members.find_one({"userid":userid})
+
+    member['userid'] = ret['userid']
+    member['username'] = ret['username']
+  except Exception as e:
+    mylogger.error(e)
+  
+  return json.dumps(member)
+
+
+@blueprint.route('/modify', methods = ['POST'])
+def modifyMember():
+  result = {
+    'status': 'success'
+  }
+  try:
+    ret = db.members.find_one_and_update({"userid":request.form['userid']}, 
+      {'$set' : {"username":request.form['username']}
+      }, return_document=ReturnDocument.AFTER)
+
+  except Exception as e:
+    mylogger.error(e)
+    result['status'] = 'fail'
+  
+  members = db.members.find()
+
+  return render_template('member/list.html', members = members)
+
+
 @blueprint.route('/<userid>', methods = ['DELETE'])
 def remove(userid):
-  # To do
   mylogger.info(userid + ' is deleted')
 
   result = {
